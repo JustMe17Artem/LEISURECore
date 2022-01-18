24e541f45573bbf7bfb7fa9927575231bf5ad2d0
h4irullin.adel@yandex.ru <h4irullin.adel@yandex.ru>
2022-01-18 19:39:05 +0300

    заметки от Гульназ

--- DesktopLEISURE/MainWindow.xaml.cs
+++ DesktopLEISURE/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LEISURECore;
+//много неиспользуемых библиотек, надо стереть ненужные
 
 namespace DesktopLEISURE
 {
